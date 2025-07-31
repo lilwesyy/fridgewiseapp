0fec617cf1e2234e4aee04fb548a839d3ee85ae9
Mirco <mirco.carp@icloud.com>
2025-07-31 05:39:55 +0200

    test

--- frontend/web/src/components/Download.tsx
+++ frontend/web/src/components/Download.tsx
@@ -71,7 +71,7 @@ export default function Download() {
                 <span className="block">la tua cucina?</span>
               </h2>
               <p className="text-xl text-green-100 leading-relaxed">
-                Scarica Adessssosososso FridgeWise oggi e inizia a cucinare in modo più intelligente, 
+                Scarica Adesso e mai più FridgeWise oggi e inizia a cucinare in modo più intelligente, 
                 sostenibile e delizioso.
               </p>
             </div>
